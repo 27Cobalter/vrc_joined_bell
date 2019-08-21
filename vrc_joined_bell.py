69b03b6c93b7f6e4204b061872fd51aad8dce95a
27Cobalter <kontyanzhihg@gmail.com>
2019-08-21 18:04:27 +0900

    [fix]既存のインスタンスに入ったときに多重再生される問題修正
    既存のインスタンスに入った場合に人数分OnPlayerJoinedログが流れるので
    同じ時間(秒オーダ)に同じ種類の通知が来た場合1回だけ音声再生するように変更

--- vrc_joined_bell.py
+++ vrc_joined_bell.py
@@ -20,8 +20,10 @@ if __name__ == "__main__":
     with open("notice.yml", "r") as conf:
         notices = yaml.load(conf, Loader=yaml.SafeLoader)
     print("load config")
-    for k, v in notices.items():
-        print("  " + k + ": " + v)
+    data = {}
+    for pattern, soundfile in notices.items():
+        data[pattern] = ["", re.compile(pattern.encode()), soundfile]
+        print("  " + pattern + ": " + soundfile)
 
     vrcdir = os.environ["USERPROFILE"] + "\\AppData\\LocalLow\\VRChat\\VRChat\\"
     logfiles = glob.glob(vrcdir + "output_log_*.txt")
@@ -30,10 +32,17 @@ if __name__ == "__main__":
     with open(logfiles[0], "rb") as f:
         print("open logfile : ", logfiles[0])
         loglines = tail(f)
+
+        timereg = re.compile(
+            "([0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}) .*".encode()
+        )
+
         for line in loglines:
-            for pattern, soundfile in notices.items():
-                if re.match(pattern.encode(), line):
+            for pattern, item in data.items():
+                logtime = timereg.match(line)
+                if logtime and logtime.group(1) != item[0] and item[1].match(line):
                     print(line)
-                    with open(soundfile, "rb") as f:
-                        data = f.read()
-                    winsound.PlaySound(data, winsound.SND_MEMORY)
+                    item[0] = logtime.group(1)
+                    with open(item[2], "rb") as f:
+                        sound = f.read()
+                    winsound.PlaySound(sound, winsound.SND_MEMORY)
